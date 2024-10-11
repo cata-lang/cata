05eb7aa66e94dac90e0a95a8c7ca98750d40187e
Leo Wang <65877233+who576@users.noreply.github.com>
2024-10-11 17:24:53 -0400

    output ir to file and run compile script (#1)
    * output ir to file and run compile script
    
    * removed iostream from main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+#include <fstream>
 
 #include "codegen.h"
 #include "parser.h"
 
 int main(int argc, char* argv[]) {
+  std::ofstream output_file{"./ir/program.ll", std::fstream::trunc};
   // Tokenizer tokenizer{"./program.cata"};
   // while (Token token = tokenizer.next_token(true)) {
   //   std::cout << token << " ";
@@ -11,5 +12,8 @@ int main(int argc, char* argv[]) {
   auto parser = Parser{"./program.cata"};
   // Codegen::instance().visitNode(parser.definition().get());
   // Codegen::instance().visitNode(parser.top_level().get());
-  std::cout << Codegen::instance().get_ir() << std::endl;
+
+  // std::cout << Codegen::instance().get_ir() << std::endl;
+  output_file << Codegen::instance().get_ir() << std::endl;
+  system("cd ./ir/ && sh ./compile.sh");
 }
